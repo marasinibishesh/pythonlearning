28f7f52b341841b06ef9be600190011c28371a2e
Bishesh Marasini <marasinibishesh@gmail.com>
2024-05-24 23:38:17 +0545

    Python Internal Strucutre Foundation

---
+++ 01_basics/hello_world.py
@@ -0,0 +1,10 @@
+print("Bishesh and Dark")
+def biscal(n):
+    print(n)
+
+biscal("Bishesh and Bright")
+
+chai_one="lemon tea"
+chai_two="ginger tea"
+chai_three="masala tea"
+#from importlib import reload #for reloading the .py files
---
+++ 01_basics/world.py
@@ -0,0 +1,2 @@
+from hello_world import biscal
+biscal("Bishesh is hero")
